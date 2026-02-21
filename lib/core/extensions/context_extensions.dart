76eb24c92a0fab04abb749d8ed0d036816feb71e
Abdullah Nabil <abdallahnabil770@gmail.com>
2026-02-21 17:18:51 +0200

    -edit extensions

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,15 +1,29 @@
-import 'dart:core';
-
 import 'package:flutter/material.dart';
 
 extension ContextExtensions on BuildContext {
-  double get width => MediaQuery.of(this).size.width;
+  // responsive height
+  double hg(double n) {
+    final h = MediaQuery.of(this).size.height;
+    return n * (h / 880);
+  }
 
-  double get height => MediaQuery.of(this).size.height;
+  // responsive width
+  double wd(double n) {
+    final w = MediaQuery.of(this).size.width;
+    return n * (w / 390);
+  }
 
+  // responsive font size using textScaler
+  double fs(double fontSize, {double baseWidth = 390}) {
+    final w = MediaQuery.of(this).size.width;
+    final scaler = MediaQuery.textScalerOf(this);
+
+    // 1. احسب الحجم المتناسب مع عرض الشاشة
+    final responsiveSize = fontSize * (w / baseWidth);
+
+    return scaler.scale(responsiveSize);
+  }
+
+  // textTheme shortcut
   TextTheme get textTheme => Theme.of(this).textTheme;
-
-  double hg(double n) => n * (height / 880);
-
-  double wd(double n) => n * (width / 390);
 }
