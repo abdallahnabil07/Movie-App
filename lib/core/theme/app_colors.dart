a0f13a8d7de2e2d706dcc28c9fe2417bbfc0917b
yussef <yussef.faragalla@gmail.com>
2026-02-24 06:23:25 +0200

    + splash screen + update profile ui

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -7,6 +7,7 @@ abstract class AppColors {
   static const Color greyColor = Color(0xffA0A0A0);
   static const Color charcoalGray = Color(0xff282A28);
   static const Color redColor = Color(0xffE82626);
+  static const Color transparentColor = Colors.transparent;
   static const Color darkGreyColor = Color(0xff686868);
   static const Color lightGreyColor = Color(0xffD6D6D6);
 }
