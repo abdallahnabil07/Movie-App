cbe1a3420f5f995cbf6dca38f8f05d5609476105
Abdullah Nabil <abdallahnabil770@gmail.com>
2026-03-02 22:46:55 +0200

    - # Firebase setup done✅ - # Firebase database created (Standard Edition)✅ - # Test Mode rules active until 1 Apr 2026 ✅ - # firebase_core added to pubspec.yaml ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/local/cache_helper_.dart';
 import 'package:movie_app/core/local/constants/cache_key.dart';
@@ -5,9 +6,13 @@ import 'package:movie_app/core/theme/app_theme.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/routes/app_routes_name.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await CacheHelper.init();
   runApp(
     MyApp(),
