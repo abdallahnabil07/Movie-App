05a9c9d2d85d19dea78d5aca479ba02d7b3a4e95
yussef <yussef.faragalla@gmail.com>
2026-03-04 07:56:54 +0200

    + update_profile.dart (Logic)

--- lib/modules/layout/profile/utils/profile_firebase_service.dart
+++ lib/modules/layout/profile/utils/profile_firebase_service.dart
@@ -1,1 +1,63 @@
-abstract class ProfileFirebaseService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../../core/enum/auth_error.dart';
+
+abstract class ProfileFirebaseService {
+  static Future<void> updateProfile({
+    required String name,
+    required String phone,
+    required String avatarPath,
+  }) async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+
+      if (user == null) {
+        throw CustomError("User not logged in");
+      }
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .update({
+        'name': name,
+        'phone': phone,
+        'avatar': avatarPath,
+        'updatedAt': FieldValue.serverTimestamp(),
+      });
+
+    } catch (e) {
+      throw CustomError(e.toString());
+    }
+  }
+
+
+
+  static Future<void> deleteAccount() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+
+      if (user == null) {
+        throw CustomError("User not logged in");
+      }
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .delete();
+
+      await user.delete();
+    } on FirebaseAuthException catch (e) {
+      throw CustomError(e.message ?? "Delete failed");
+    } catch (e) {
+      throw CustomError(e.toString());
+    }
+  }
+
+
+
+
+
+
+
+}
